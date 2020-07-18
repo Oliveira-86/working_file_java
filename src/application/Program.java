ccfe8245848bda1f07067aae91bb2db5a68b52f6
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-07-17 23:50:57 -0300

    Add new class/add bw

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,68 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
 	 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
 		
 		System.out.println("Enter a folder path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
 		
+		boolean sucess = new File(strPath + "\\out").mkdir();
+		
+		String targetFile = strPath + "\\out\\summary.csv";
+		
 		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
 			String line = br.readLine();
 			while (line != null) {
-				System.out.println(line);
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+				
+				list.add(new Product(name, price, quantity));
+				
 				line = br.readLine();
 				}
+			
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))){
+				
+				for(Product items : list) {
+					bw.write(items.getName() + ", " + String.format("%.2f", items.total()));
+					bw.newLine();
+				}
+				
+					System.out.println(strPath + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
 		}
-		catch(IOException e){
-			System.out.println("ERROR: " + e.getMessage());
-		}
+
+		sc.close();
 	}
 }
